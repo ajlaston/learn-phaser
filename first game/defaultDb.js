f095e85998cea866f8f55fbea832e78c57c48e35
Adam Laston <30796595+ajlaston@users.noreply.github.com>
2021-01-25 09:54:55 -0600

    Create defaultDb.js

---
+++ first game/defaultDb.js
@@ -0,0 +1,21 @@
+//db default object that is not instantiated
+//
+
+'use strict';
+
+//inteface, methods etc
+class defaultDb {
+    
+    items = class {
+        static clothes = []
+        static tools = []
+        static junk = []
+        static electronics = []
+
+    }
+
+    
+    
+}
+
+export default defaultDb;
